ad116794d3b7950579bc729b852c15af97a6c67e
AmazingBongo811 <aron.lakatos123@gmail.com>
2025-04-14 12:49:17 +0200

    restrucuture ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,36 +8,18 @@ class Ticket extends Model
 {
     protected $fillable = [
         'order_id',
-        'festival_id',
-        'detail_ticket_id', // For example specific band, restaurant
+        'event_id',
         'qr_code',
     ];
 
-    public function festival()
+    // Define the relationship with the Event model
+    public function event()
     {
-        return $this->belongsTo(Festival::class);
+        return $this->belongsTo(Event::class, 'event_id');
     }
 
     public function order()
     {
         return $this->belongsTo(Order::class);
-    }
-
-    public function detail_ticket()
-    {
-        $festival = $this->festival();
-
-        switch($festival->festivalType)
-        {
-            case 0:
-                return JazzFestival::where('festival_id', $festival->id)->get();
-            case 1:
-                return null; // Replace with restaurants!!!!
-            case 2:
-                return null; // Replace with history!!!!
-
-            default:
-                throw new \Exception('Unsupported Festival type');
-        }
     }
 }
