5a0ec3b36964e8af6f2e55148a799abd318fd219
ayazspv <ayazsaadatpour2@gmail.com>
2025-05-21 19:30:43 +0200

    Yummy homepage design restaurants section (section 3/4)

--- resources/js/Pages/Festivals/Yummy.vue
+++ resources/js/Pages/Festivals/Yummy.vue
@@ -53,13 +53,74 @@ const props = defineProps<{
         </div>
     </div>
     <div class="restaurants-list">
-
+        <h2>
+            You may like one of the these amazing restaurants:
+        </h2>
+        <div class="filters">
+            <p>
+                Restaurants Filter:
+            </p>
+            <div class="filter-items">
+                <button class="btn btn-primary">All</button>
+                <button class="btn btn-outline-primary">French</button>
+            </div>
+        </div>
+        <div class="restaurants">
+            <div class="restaurant">
+                <img src="../../../../storage/app/public/main/yummy/top-view-table-full-food.jpg" alt="">
+                <h1>Cafe de Roemer</h1>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="line"></div>
+                <div class="food-type">
+                    <p class="bold-part">food type:</p>
+                    <p class="normal-part">French</p>
+                </div>
+                <div class="seats">
+                    <p class="bold-part">seats:</p>
+                    <p class="normal-part">2</p>
+                </div>
+                <div class="line"></div>
+                <div class="features">
+                    <div class="feature-item">
+                        <i class="fa-brands fa-accessible-icon"></i>
+                        <p>Feature 1</p>
+                    </div>
+                    <div class="feature-item">
+                        <i class="fa-brands fa-accessible-icon"></i>
+                        <p>Feature 1</p>
+                    </div>
+                    <div class="feature-item">
+                        <i class="fa-brands fa-accessible-icon"></i>
+                        <p>Feature 1</p>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="extra-information">
+                    <p>📍</p>
+                    <p>Botermarkt 17, 2011 XL Haarlem</p>
+                </div>
+                <div class="extra-information">
+                    <p>🕕</p>
+                    <p>From 18:00</p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="footer">
 
     </div>
 </template>
 <style scoped>
+p, h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+}
+
 .header {
     display: flex;
     padding: 25px 0px 150px 0px;
@@ -124,6 +185,7 @@ const props = defineProps<{
     font-style: normal;
     font-weight: 500;
     line-height: normal;
+    margin-bottom: 1rem;
 }
 
 .hero-content button {
@@ -198,5 +260,144 @@ const props = defineProps<{
     font-family: Bayon;
     font-size: 40px;
     margin: 0;
+}
+
+.restaurants-list {
+    display: flex;
+    padding: 50px 0px;
+    flex-direction: column;
+    align-items: center;
+    gap: 25px;
+    align-self: stretch;
+    background: linear-gradient(0deg, rgba(233, 233, 233, 0.95) 0%, rgba(233, 233, 233, 0.95) 100%), url(../../../../storage/app/public/main/yummy/top-view-pasta-waffles-with-copy-space.jpg) lightgray 50% / cover no-repeat;
+}
+
+.restaurants-list h2 {
+    color: #000;
+    text-align: center;
+    font-family: Bayon;
+    font-size: 50px;
+}
+
+.filters {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0 200px;
+}
+
+.filters p {
+    color: #000;
+    text-align: center;
+    font-family: NotoSansTamil;
+    font-size: 25px;
+}
+
+.filter-items {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+}
+
+.restaurants {
+        display: flex;
+    width: 100%;
+    align-items: flex-start;
+    align-content: flex-start;
+    gap: 50px 50px;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+.restaurant {
+    display: flex;
+    width: 375px;
+    padding: 25px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 15px;
+    border-radius: 15px;
+    border: 1px solid #ABABAB;
+    background: rgba(255, 255, 255, 0.38);
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.15);
+    backdrop-filter: blur(10px);
+}
+
+.restaurant img {
+    max-height: 250px;
+    border-radius: 15px;
+    border: 1px solid #000;
+}
+
+.restaurant h1 {
+    color: #F8A219;
+    font-family: Bayon;
+    font-size: 30px;
+    margin: 0;
+}
+
+.stars {
+    display: flex;
+    align-items: flex-start;
+}
+
+.stars i {
+    color: #F8A219;
+    font-size: 20px;
+}
+
+.line {
+    width: 100%;
+    height: 2px;
+    background: #F8A219;
+}
+
+.food-type, .seats {
+        display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5px;
+}
+
+.bold-part {
+    color: #000;
+    font-family: Bayon !important;
+    font-size: 18px !important;
+}
+
+.normal-part {
+    color: #000;
+    font-family: NotoSansTamil;
+    font-size: 14px;
+}
+
+.features {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+}
+
+.feature-item {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 10px;
+}
+.feature-item i {
+    font-size: 25px;
+}
+
+.restaurant p {
+    color: #000;
+    font-family: NotoSansTamil;
+    font-size: 14px;
+}
+
+.extra-information {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5px;
 }
 </style>
